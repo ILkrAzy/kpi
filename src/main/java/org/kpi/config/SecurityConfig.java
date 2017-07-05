f3a9ae2d82f5b0d99fd8944da7f82996669d250d
vquochuy <quochuy0925@gmail.com>
2017-07-05 14:40:19 +0700

    [Security Basic] Fix form login

--- src/main/java/org/kpi/config/SecurityConfig.java
+++ src/main/java/org/kpi/config/SecurityConfig.java
@@ -2,6 +2,10 @@ package org.kpi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -9,11 +13,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * Created by lnphi on 7/4/2017.
  */
 @Configuration
-public class SecurityConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        return encoder;
-    }
-}
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().
+				antMatchers(HttpMethod.POST, "/api/**").authenticated()
+				.antMatchers(HttpMethod.PUT, "/api/**").authenticated()
+				.antMatchers(HttpMethod.DELETE, "/api/**")
+				.authenticated().anyRequest().permitAll().and().httpBasic().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+}
