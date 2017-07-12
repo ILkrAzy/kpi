68039f1010ec6a480874000c0a030d12c25f85c0
lnphi <phi.luunhat@gmail.com>
2017-07-12 16:04:53 +0700

    login with JWT

---
+++ src/main/java/org/kpi/config/security/AccountCredentials.java
@@ -0,0 +1,16 @@
+package org.kpi.config.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by lnphi on 7/12/2017.
+ */
+public class AccountCredentials {
+    @Getter
+    @Setter
+    private String username;
+    @Getter
+    @Setter
+    private String password;
+}
