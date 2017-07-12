5b786a640501e65092646d5da96b39a493d7b536
lnphi <phi.luunhat@gmail.com>
2017-07-12 09:46:31 +0700

    add unit test for UserService

--- src/test/java/org/kpi/service/UserServiceImplTest.java
+++ src/test/java/org/kpi/service/UserServiceImplTest.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -57,28 +57,66 @@ public class UserServiceImplTest {
 
     @Test
     public void getByUsername() throws Exception {
-        User ddlanh = new User();
-        ddlanh.setUsername("ddlanh");
-        ddlanh.setFirstName("Lanh");
-        ddlanh.setLastName("Dang");
-        ddlanh.setEmail("test@gmail.com");
-        ddlanh.setId(1);
+        User ddlanh = getSampleUser();
 
         when(userRepository.findByUsername(ddlanh.getUsername())).thenReturn(ddlanh);
         assertThat(service.getByUsername(ddlanh.getUsername()), equalTo(ddlanh));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void findByUsernameShouldThrowExceptionWhenUsernameDoesNotExist() throws Exception {
+        String username = "username";
+        when(userRepository.findByUsername(username)).thenReturn(null);
+        service.getByUsername(username);
+    }
+
+    @Test
+    public void existReturnFalseWhenUsernameDoesNotExist() throws Exception {
+        String username = "username";
+        when(userRepository.findByUsername(username)).thenReturn(null);
+        assertFalse(service.exist(username));
+    }
+
+    @Test
+    public void existReturnTrueWhenUsernameExist() throws Exception {
+        User ddlanh = getSampleUser();
+        String username = ddlanh.getUsername();
+        when(userRepository.findByUsername(username)).thenReturn(ddlanh);
+        assertTrue(service.exist(username));
+    }
+
+    @Test
+    public void existEmailReturnTrueWhenEmailExist() throws Exception {
+        User ddlanh = getSampleUser();
+        String email = ddlanh.getEmail();
+        when(userRepository.findByEmail(email)).thenReturn(ddlanh);
+        assertTrue(service.existEmail(email));
+    }
+
+    @Test
+    public void existEmailReturnFalseWhenEmailDoesNotExist() throws Exception {
+        User ddlanh = getSampleUser();
+        String email = ddlanh.getEmail();
+        when(userRepository.findByEmail(email)).thenReturn(null);
+        assertFalse(service.existEmail(email));
+    }
+
     @Test
     public void save() throws Exception {
-        User ddlanh = new User();
-        ddlanh.setUsername("ddlanh");
-        ddlanh.setFirstName("Lanh");
-        ddlanh.setLastName("Dang");
-        ddlanh.setPassword("123456");
-        ddlanh.setEmail("test@gmail.com");
-        ddlanh.setId(1);
+        User ddlanh = getSampleUser();
         service.save(ddlanh);
         verify(userRepository, Mockito.times(1)).save(ddlanh);
         assertThat(ddlanh.getPassword(), equalTo("123456"));
+    }
+
+    private User getSampleUser() {
+        User user = new User();
+        user.setUsername("ddlanh");
+        user.setFirstName("Lanh");
+        user.setLastName("Dang");
+        user.setPassword("123456");
+        user.setEmail("test@gmail.com");
+        user.setId(1);
+        return user;
     }
 }
