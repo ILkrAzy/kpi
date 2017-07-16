e8afa80c172bd93ec8398633d53f37de988660af
Cuong <cuongnx2015@gmail.com>
2017-07-16 13:58:48 +0700

    Use External Properties file

--- src/main/java/org/kpi/Application.java
+++ src/main/java/org/kpi/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Configuration
+@PropertySources({
+@PropertySource(value = "file:${user.home}/Kpi.properties", ignoreResourceNotFound = true),
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true) })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
