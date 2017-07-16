8c67b0f3aa5c8c76dd7bcf5995872a2a329a3bd5
Cuong <cuongnx2015@gmail.com>
2017-07-16 22:44:43 +0700

    Use External Properties file

--- src/main/java/org/kpi/Application.java
+++ src/main/java/org/kpi/Application.java
@@ -2,6 +2,9 @@ package org.kpi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @Configuration
