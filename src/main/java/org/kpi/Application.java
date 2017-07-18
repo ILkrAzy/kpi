b185aab9ceab4dfeecdb3e585b315989a4080dab
Xuan Cuong <cuongnx2015@gmail.com>
2017-07-18 18:16:37 +0700

    Update and fix Use External Properties file

--- src/main/java/org/kpi/Application.java
+++ src/main/java/org/kpi/Application.java
@@ -1,18 +1,28 @@
 package org.kpi;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@Configuration
-@PropertySources({
-@PropertySource(value = "file:${user.home}/Kpi.properties", ignoreResourceNotFound = true),
-@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true) })
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // Path of the Kpi.properties in user.home
+        String path = System.getProperty("user.home") + "/kpi.properties";
+
+        File f = new File(path);
+        if (f.canRead()) {
+            FileInputStream propertiesFile = new FileInputStream(path);
+            Properties p = new Properties(System.getProperties());
+            p.load(propertiesFile);
+            // set the system properties
+            System.setProperties(p);
+        }
         SpringApplication.run(Application.class, args);
+
     }
 }
