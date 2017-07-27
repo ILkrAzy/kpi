e9360896299587f642663ca280bc7b24a10e2939
vquochuy <quochuy0925@gmail.com>
2017-07-27 13:54:42 +0700

    Document for get user. Run command: gradlew asciidoc -> generated files html in build/asciidoc/html5/

--- src/test/java/org/kpi/controller/UserControllerTest2.java
+++ src/test/java/org/kpi/controller/UserControllerTest2.java
@@ -1,7 +1,9 @@
 package org.kpi.controller;
+
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,18 +13,20 @@ import org.kpi.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=Application.class)
+@SpringBootTest(classes = Application.class)
 public class UserControllerTest2 {
     @Rule
     public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("build/generated-snippets");
-    
-    private MockMvc mockMvc;
+
+    private MockMvc               mockMvc;
 
     @Autowired
     private WebApplicationContext context;
@@ -30,15 +34,27 @@ public class UserControllerTest2 {
     @Before
     public void setUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-                .apply(documentationConfiguration(this.restDocumentation)) 
+                .apply(documentationConfiguration(this.restDocumentation))
                 .build();
     }
-    
+
     @Test
-    public void getAll() throws Exception{
-        this.mockMvc.perform(get("/api/users").accept(MediaType.APPLICATION_JSON)) 
-        .andExpect(status().isOk())
-        .andDo(document("index"));
+    public void getAll() throws Exception {
+        FieldDescriptor[] user = new FieldDescriptor[] {
+                fieldWithPath("username").description("The user's username"),
+                fieldWithPath("email").description("The user's email address"),
+                fieldWithPath("firstName").description("The user's last name"),
+                fieldWithPath("lastName").description("The user's last name") };
+
+        this.mockMvc
+                .perform(get("/api/users").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document(
+                        "users",
+                        responseFields(
+                                fieldWithPath("[]").description(
+                                        "An array of users")).andWithPrefix(
+                                "[].", user)));
     }
-    
+
 }
